3433b651ac6205357ba22c85dc79ae80d61b21df
danielsys <daniel@iamazon.com.br>
2018-10-08 14:51:57 -0300

    Inicio projeto

--- _bottom.php
+++ _bottom.php
@@ -1,7 +1,7 @@
 </div> <!-- /.container -->
 
 
-            <!-- FOOTER -->
+            <!-- FOOTER  -->
             <footer class="container">
                 <!--<p class="float-right"><a href="#">Back to top</a></p> -->
                 <!-- <p>dev by Daniel Lima <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p> -->
